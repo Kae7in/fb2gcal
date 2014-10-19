bb305d0931d5f27b5fbd6ceadac6a9edd2a39bf7
Kaelin <kae7in@gmail.com>
2014-10-18 19:18:17 -0500

    fetching both Facebook and Google Calendars

--- fb2gcal.py
+++ fb2gcal.py
@@ -1,8 +1,10 @@
 import facebook
 import requests
+from apiclient.discovery import build
+import httplib2
+from oauth2client.client import AccessTokenCredentials
 # from oauth2client import 
 from secrets import *
-
 user = "josh.kelle"
 
 graph = facebook.GraphAPI(facebook_access_token)
@@ -11,11 +13,26 @@ events = graph.get_connections(profile['id'], 'events')
 
 # Perform some action on each post in the collection we receive from
 # Facebook.
+print("Facebook Events:")
 for event in events['data']:
-	print(event['name'])
+	print(" - " + event['name'])
 
 # Attempt to make a request to the next page of data, if it exists.
 events = requests.get(events['paging']['next']).json()
 # When there are no more pages (['paging']['next']), break from the
 # loop and end the script.
+
+ghttp = httplib2.Http()
+gcred = AccessTokenCredentials(google_access_token, 'fb2gcal hackathon project')
+gcred.authorize(ghttp)
+
+service = build("calendar", "v3", http=ghttp)
+
+calendars = service.calendarList().list().execute()
+
+print("\nGoogle Calendars:")
+for calendar in calendars['items'] :
+	print (" - " + calendar['summary'])
+
+
 
