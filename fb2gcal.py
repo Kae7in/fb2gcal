a0c29f36235b6f4e13006cfda2a05d471651f7f4
Kaelin <kae7in@gmail.com>
2014-10-18 21:19:43 -0500

    successfully porting Facebook events to Google Calendar

--- fb2gcal.py
+++ fb2gcal.py
@@ -5,6 +5,9 @@ import httplib2
 from oauth2client.client import AccessTokenCredentials
 # from oauth2client import 
 from secrets import *
+from pprint import pprint
+from datetime import datetime, timedelta
+
 user = "josh.kelle"
 
 graph = facebook.GraphAPI(facebook_access_token)
@@ -18,7 +21,7 @@ for event in events['data']:
 	print(" - " + event['name'])
 
 # Attempt to make a request to the next page of data, if it exists.
-events = requests.get(events['paging']['next']).json()
+#events = requests.get(events['paging']['next']).json()
 # When there are no more pages (['paging']['next']), break from the
 # loop and end the script.
 
@@ -33,6 +36,60 @@ calendars = service.calendarList().list().execute()
 print("\nGoogle Calendars:")
 for calendar in calendars['items'] :
 	print (" - " + calendar['summary'])
+	if calendar['summary'] == "Facebook Events":
+		fbEventCalendar = calendar
 
+"""
+1. for each fb event
+	1. make sure it's not already in any calendar
+	2. add it to the calendar
+"""
 
+def eventInCalendars(event, calendars):
+	for calendar in calendars:
+		if eventInCalendar(event, calendar):
+			return True
+	return False
+
+def eventInCalendar(event, calendar):
+	# TODO: make this actually work
+	return False
+
+def addEventToCalendar(event, calendar):
+	# print "type(event) =", type(event)
+
+	timezone = event["start_time"][-5:]
+	timezone = timezone[:-2] + ":" + timezone[-2:]
+
+	def parseFacebookTime(s):
+		return datetime.strptime(s[:-5], '%Y-%m-%dT%H:%M:%S')
+
+	def formatGoogleTime(dt):
+		s = datetime.strftime(dt, '%Y-%m-%dT%H:%M:%S.000')
+		return s + timezone
+
+	event_name = event['name']
+	start_time = parseFacebookTime(event['start_time'])
+	end_time = parseFacebookTime(event['end_time']) if 'end_time' in event else start_time + timedelta(hours=1)
+
+	body = {
+		'summary' : event_name,
+		'start' : {
+			'dateTime' : formatGoogleTime(start_time)
+		},
+		'end' : {
+			'dateTime' : formatGoogleTime(end_time)
+		}
+	}
+
+	print "making request: ", body
+
+	created_event = service.events().insert(calendarId=targetCalendar, body=body).execute()
+
+# print "events"
+# pprint(events)
+
+for event in events['data']:
+	if not eventInCalendars(event, calendars):
+		addEventToCalendar(event, fbEventCalendar)
 
